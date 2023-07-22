f201f61a26e915d0c2f1f2d6a3e5e426016d9a5f
Ip <127031847+Etsehiywot@users.noreply.github.com>
2023-07-22 10:31:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,25 @@
 import requests
-import time
 from bs4 import BeautifulSoup
 
-url = 'https://www.capitalethiopia.com/'
+url = 'https://www.booksiteethiopia.com'
 response = requests.get(url)
 content = response.content
-def scrape_data(content):
 
-  soup = BeautifulSoup(content, 'html.parser')
-  titles = soup.find_all("div", class_="col-md-4 col-sm-6 col-xs-6 simple")
-  articles = soup.find_all("article")
-  
-  
-  for article in articles:
-   image_tag = article.find("img")
-   image_url = image_tag["src"] if image_tag else "Image URL not found"
-    
-   text_tag = article.find("h3", itemprop="name").find("a")
-   text = text_tag.text.strip() if text_tag else "Text not found"
+def scrape_data(html_code):
+    soup = BeautifulSoup(html_code, "html.parser")
+    articles = soup.find_all("li", attrs={"class":"product"})
 
-   
+    for article in articles:
+        image_tag = article.find("img")
+        image_url = image_tag["src"] if image_tag else "Image URL not found"
 
+        text_tag = article.find("h2",attrs={"class":"woocommerce-loop-product__title"})
+        text = text_tag.text.strip() if text_tag else "Text not found"
 
-   base_url = 'https://api.telegram.org/bot6342347387:AAEWK6_qJ21dTaYCY-Zbgie_fiY2D1DUCjo/sendMessage?chat_id=878856703&text='
-   message = f"{text}\nImage URL: {image_url}"
-   requests.get(base_url + message)
-# time.sleep(10)
-   print(text)
-   print(image_url)
+        base_url = 'https://api.telegram.org/bot6342347387:AAEWK6_qJ21dTaYCY-Zbgie_fiY2D1DUCjo/sendMessage?chat_id=-1001940209421&text='+text+image_url
+        requests.get(base_url)
+      #   print("Image URL:", image_url)
+        print("Text:", text)
+        
+
 scrape_data(content)
- 
